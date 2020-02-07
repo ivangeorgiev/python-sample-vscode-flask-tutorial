8a43dfce1e577823c97a7993ad749db23155d9b8
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-02-07 12:56:09 +0100

    Added IDs to nav links

--- tests/ui/search.feature
+++ tests/ui/search.feature
@@ -6,3 +6,7 @@ Feature: Search for websites
     And submit 'seleniumhq' query
     Then The browser returns search results
 
+    Scenario: site is up
+    Given the browser is opened
+    When I navigate to "https://python-sample-flask-13382.azurewebsites.net/"
+    And click "nav-home" navigates to page "Home"
--- tests/ui/test_google.py
+++ tests/ui/test_google.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from pytest_bdd import scenario, given, when, then, parsers
+import pytest
 
-
-
+#@pytest.mark.skip(reason="no way of currently testing this")
 @scenario("./search.feature", "submit search query returns list of results")
 def test_submit_search_query_returns_a_list_of_results():
     pass
 
+@pytest.mark.skip(reason="no way of currently testing this")
+@scenario("./search.feature", "site is up")
+def test_site_is_up():
+    pass
 
 @given("the browser is opened", scope="session")
 def browser():
@@ -17,6 +21,12 @@ def browser():
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
+
+@when(parsers.parse('click "{element_id:string}" navigates to page "{page_title:string}"',
+        extra_types=dict(string=str)))
+def click_link_opens_page(browser: webdriver.Chrome, element_id, page_title):
+    element = browser.find_element_by_id(element_id)
+    element.click()
 
 @when(parsers.parse('I navigate to "{url:string}"', extra_types=dict(string=str)))
 def navigate_page(browser, url):
